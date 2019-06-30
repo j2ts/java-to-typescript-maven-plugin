12be6d4ec99b66ae4abc4c4ce9a518c54d6973b5
Jens Brimfors <jens.brimfors@nordnet.se>
2019-06-30 07:32:12 +0200

    Use sonar for code coverage and quality checks

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,3 +5,4 @@ set -e
 mvn clean install -s ./.travis/maven_settings.xml -e
 mvn clean package -f example/pom.xml -s ./.travis/maven_settings.xml -e
 java -jar example/target/example-0.0.0-SNAPSHOT-jar-with-dependencies.jar
+mvn sonar:sonar
